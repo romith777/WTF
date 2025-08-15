e36bd00b1086254da3a45d9653bea7fad5dca018
romith777 <romithpagadala123@gmail.com>
2025-08-15 15:50:57 +0530

    updated products

--- data/products.js
+++ data/products.js
@@ -90,9 +90,100 @@ const tees = [{
     keyword : ["tees","tshirts","tee","tshirt"]
 }
 ]
-const cargos = [
 
+const cargos = [
+{
+    id : "cargo-1",
+    name : "cargos",
+    image : "../assets/cargo.jpg",
+    about : "This is a Cargo, yes it is",
+    brandName : "WTPrints",
+    priceCents : 3000,
+    keyword : ["cargos","pants","trousers"]
+},
+{
+    id : "cargo-2",
+    name : "cargos",
+    image : "../assets/cargo.jpg",
+    about : "This is a Cargo, yes it is",
+    brandName : "WTPrints",
+    priceCents : 3000,
+    keyword : ["cargos","pants","trousers"]
+},
+{
+    id : "cargo-3",
+    name : "cargos",
+    image : "../assets/cargo.jpg",
+    about : "This is a Cargo, yes it is",
+    brandName : "WTPrints",
+    priceCents : 3000,
+    keyword : ["cargos","pants","trousers"]
+},
+{
+    id : "cargo-4",
+    name : "cargos",
+    image : "../assets/cargo.jpg",
+    about : "This is a Cargo, yes it is",
+    brandName : "WTPrints",
+    priceCents : 3000,
+    keyword : ["cargos","pants","trousers"]
+},
+{
+    id : "cargo-5",
+    name : "cargos",
+    image : "../assets/cargo.jpg",
+    about : "This is a Cargo, yes it is",
+    brandName : "WTPrints",
+    priceCents : 3000,
+    keyword : ["cargos","pants","trousers"]
+},
+{
+    id : "cargo-6",
+    name : "cargos",
+    image : "../assets/cargo.jpg",
+    about : "This is a Cargo, yes it is",
+    brandName : "WTPrints",
+    priceCents : 3000,
+    keyword : ["cargos","pants","trousers"]
+},
+{
+    id : "cargo-7",
+    name : "cargos",
+    image : "../assets/cargo.jpg",
+    about : "This is a Cargo, yes it is",
+    brandName : "WTPrints",
+    priceCents : 3000,
+    keyword : ["cargos","pants","trousers"]
+},
+{
+    id : "cargo-8",
+    name : "cargos",
+    image : "../assets/cargo.jpg",
+    about : "This is a Cargo, yes it is",
+    brandName : "WTPrints",
+    priceCents : 3000,
+    keyword : ["cargos","pants","trousers"]
+},
+{
+    id : "cargo-9",
+    name : "cargos",
+    image : "../assets/cargo.jpg",
+    about : "This is a Cargo, yes it is",
+    brandName : "WTPrints",
+    priceCents : 3000,
+    keyword : ["cargos","pants","trousers"]
+},
+{
+    id : "cargo-10",
+    name : "cargos",
+    image : "../assets/cargo.jpg",
+    about : "This is a Cargo, yes it is",
+    brandName : "WTPrints",
+    priceCents : 3000,
+    keyword : ["cargos","pants","trousers"]
+}
 ]
+
 const hoodies = []
 const shirts = []
 const jeans = []
